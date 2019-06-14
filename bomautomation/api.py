e8ac0abdb36f37182ce2d1f86f8fc2e1b0949e59
nadorml <nado_rml@hotmail.com>
2019-06-14 10:49:39 +0200

    add new new function change BOM

--- bomautomation/api.py
+++ bomautomation/api.py
@@ -22,15 +22,34 @@ def make_bom(name,selected_items):
 			if len(so_doc.row_materials)>=1:				
 				for item in so_doc.items:
 					if item.name in selected_items:
-						row_materials=[]
-						for row in so_doc.row_materials:
-							row_dict={}
-							row_dict["item_code"]=row.item_code
-							row_dict["item_name"]=row.item_name
-							row_dict["uom"]=row.uom
-							row_dict["qty"]=row.qty*(item.qty/so_doc.total_qty)
-							row_materials.append(row_dict)
-						create_bom(item.item_code,row_materials,so_doc.company,so_doc.currency,item.qty,so_doc.name)
+						res=validate_avaialable_bom(name,item.name)
+						if not res==False:
+							bom_doc=frappe.get_doc("BOM",res)
+							idx=len(bom_doc.items)+1
+							for row in so_doc.row_materials:
+								count=0
+								for bom_item in bom_doc.items:
+									if bom_item.item_code==row.item_code:
+										count += 1
+								if count==0:
+									add_bom_item(row.item_code,row.item_name,row.uom,row.qty*(item.qty/so_doc.total_qty),res,idx)
+									idx += 1
+									
+							bom_doc_final=frappe.get_doc("BOM",res)
+							bom_doc_final.save()
+							msg='BOM '+'<a href="#Form/BOM/'+bom_doc_final.name+'">'+bom_doc_final.name+'</a>'+' Updated'
+							frappe.msgprint(msg);
+						
+						else:
+							row_materials=[]
+							for row in so_doc.row_materials:
+								row_dict={}
+								row_dict["item_code"]=row.item_code
+								row_dict["item_name"]=row.item_name
+								row_dict["uom"]=row.uom
+								row_dict["qty"]=row.qty*(item.qty/so_doc.total_qty)
+								row_materials.append(row_dict)
+							create_bom(item.item_code,row_materials,so_doc.company,so_doc.currency,item.qty,so_doc.name)
 			else:
 				frappe.throw("Row Materials Required For Generate BOM")
 		else:
@@ -38,6 +57,17 @@ def make_bom(name,selected_items):
 
 	except Exception as e:
 		error_log=app_error_log(frappe.session.user,str(e))
+
+def validate_avaialable_bom(so_no,si_no):
+	so_item=frappe.get_doc("Sales Order Item",si_no)
+	bom_details=frappe.db.sql("""select name from `tabBOM` 
+		where sales_order=%s and item=%s and docstatus=0 limit 1"""
+		,(so_no,so_item.item_code),as_dict=1)
+	if len(bom_details)>=1:
+		return bom_details[0].name
+	else:
+		return False
+		
 
 def create_bom(item,item_details,company,currency,qty,sales_order):
 	try:
@@ -57,3 +87,18 @@ def create_bom(item,item_details,company,currency,qty,sales_order):
 	except Exception as e:
 		frappe.errprint(json.dumps(item_details))
 		error_log=app_error_log(frappe.session.user,str(e))
+
+def add_bom_item(item_code,item_name,uom,qty,name,idx):
+	doc=frappe.get_doc(dict(
+		doctype="BOM Item",
+		parent=name,
+		parenttype="BOM",
+		parentfield="items",
+		item_code=item_code,
+		item_name=item_name,
+		uom=uom,
+		qty=qty,
+		rate=0,
+		idx=idx
+	)).insert()
+
